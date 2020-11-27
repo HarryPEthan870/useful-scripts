28b2f6a273853315b7983bfa65968aad4cb6ef31
HarryPEthan870 <65193101+HarryPEthan870@users.noreply.github.com>
2020-11-27 10:51:48 -0500

    Add files via upload
    Minor tweaks to gpa.py

--- Python/gpa.py
+++ Python/gpa.py
@@ -5,42 +5,55 @@ numtest = input("Enter Number Of classes you have taken ")
 numpresub = int(numtest)
 num = numpresub - 1
 while num >= 0:
-    print(num)
-    varstr = input("Enter average of  ")
+    varstr = input("Enter average of a class ")
     varint = int(varstr)
     num -=1
     var = var + varint
-totalvaravr = var / numpresub
-print ("your avarge from all your classes is " + str(totalvaravr))
+totalvarvar = var / numpresub
+totalvaravr = round(totalvarvar,2)
 #letter grade calulation
-if totalvaravr == 100:
-    print("You have an A+!")
-elif totalvaravr >= 95:
-    print("You have an A!")
-elif totalvaravr >= 90:
-    print("You have an A-!")
-elif totalvaravr >= 89:
-    print("You have a B+!")
-elif totalvaravr >= 85:
-    print("You have a B!")
-elif totalvaravr >= 80:
-    print("You have a B-!")
-elif totalvaravr >= 79:
-    print("You have a C+!")
-elif totalvaravr >= 75:
-    print("You have a C!")
-elif totalvaravr >= 70:
-    print("You have a C-!")
-elif totalvaravr >= 69:
-    print("You have a D+!")
-elif totalvaravr >= 67:
-    print("You have a D!")
-elif totalvaravr >= 65:
-    print("You have a D-!")
-elif totalvaravr <= 65:
-    print("You have a F!")
+def letter_grade():
+    if totalvaravr == 100:
+        f.write("\nYou have an A+!")
+    elif totalvaravr >= 95:
+        f.write("\nYou have an A!")
+    elif totalvaravr >= 90:
+        f.write("\nYou have an A-!")
+    elif totalvaravr >= 89:
+        f.write("\nYou have a B+!")
+    elif totalvaravr >= 85:
+        f.write("\nYou have a B!")
+    elif totalvaravr >= 80:
+        f.write("\nYou have a B-!")
+    elif totalvaravr >= 79:
+        f.write("\nYou have a C+!")
+    elif totalvaravr >= 75:
+        f.write("\nYou have a C!")
+    elif totalvaravr >= 70:
+        f.write("\nYou have a C-!")
+    elif totalvaravr >= 69:
+        f.write("\nYou have a D+!")
+    elif totalvaravr >= 67:
+        f.write("\nYou have a D!")
+    elif totalvaravr >= 65:
+        f.write("\nYou have a D-!")
+    elif totalvaravr <= 65:
+        f.write("\nYou have a F!")
 #gpa calculation
 x = totalvaravr - 70
 y = x * 0.067
 z = y + 2
-print("Your GPA is " + str(z))
+zv2 = str(z)
+gpa = round(zv2,3)
+#export to file var
+def export_grades():
+    f.write("your average from all your classes is " + str(totalvaravr))
+    letter_grade()
+    f.write("\nYour GPA is " + gpa)
+#export to file
+f = open("grades.txt", "w")
+export_grades()
+#print
+f.close()
+f = open("grades.txt", "r")
+print(f.read())
